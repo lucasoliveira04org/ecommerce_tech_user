e4b5889156191c4d65e305f26e288d9d19f66e82
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-03-14 15:13:17 -0300

    feat : Exibindo produtos de teste

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,4 +1,5 @@
 import { HeaderComponent } from "../components/_header"
+import { MainProducts } from "./main-products"
 
 export const HomePage = () => {
     const navigation_header = [
@@ -22,12 +23,29 @@ export const HomePage = () => {
 
     ]
 
+    const product_data = [
+        { name: "Produto 1", description: "Descrição do produto 1", price: 100 },
+        { name: "Produto 2", description: "Descrição do produto 2", price: 200 },
+        { name: "Produto 3", description: "Descrição do produto 3", price: 300 },
+        { name: "Produto 4", description: "Descrição do produto 4", price: 400 },
+        { name: "Produto 5", description: "Descrição do produto 5", price: 500 },
+    ]
+
     return(
         <div>
             <HeaderComponent
                 navigation={navigation_header}
                 buttons={buttons_header}
             />
+
+            <div className="flex justify-center place-items-center w-full border h-[450px] bg-gray-200">
+                Carrosel de imagens
+            </div>
+
+            <MainProducts
+                product_data={product_data}
+            />
+
         </div>
     )
 }
